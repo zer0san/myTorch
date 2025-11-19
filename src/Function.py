8ed75422e133691e486c1a63030d2392860503b7
zer0san <1323639983@qq.com>
2025-11-19 14:38:10 +0800

    增添基本反向传播

--- src/Function.py
+++ src/Function.py
@@ -1,4 +1,5 @@
 from src.Variable import Variable
+import numpy as np
 
 
 class Function:
@@ -6,10 +7,42 @@ class Function:
     __call__方法是一个特殊的python方法，定义了这个方法后，
     当f=Function()时，就可以通过编写f(...)来调用__call__方法了
     '''
+
     def __call__(self, input):
         x = input.data
-        output = Variable(self.forward(x))
+        self.input = input  # 保存输入的变量
+        y = self.forward(x)
+        output = Variable(y)
         return output
 
     def forward(self, x):
-        raise NotImplementedError
+        '''
+        抛出异常，告诉使用了Function类的forward的人，这个方法应该通过继承来实现
+        '''
+        raise NotImplementedError()
+
+    # gy 是链式传播过程中，前一步计算出的导数(链式法则)
+    def backward(self, gy):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = x * 2 * gy
+        return gx
+
+
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
--- src/Variable.py
+++ src/Variable.py
@@ -1,4 +1,9 @@
+
+# 支持反向传播的变量类
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
+
+
 
