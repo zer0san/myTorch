691a8798ee6e30402d13620976b1ed7636536c93
zer0san <1323639983@qq.com>
2025-11-20 17:23:51 +0800

    封装对Config属性的修改，实现了with块控制反向传播是否进行

---
+++ src/context.py
@@ -0,0 +1,16 @@
+# 此文件中的函数，用于修改Config中的属性
+import contextlib
+from Config import Config
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value) # 更新状态
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value) # 恢复状态
+
+# 定义no_grad()函数，方便调用
+def no_grad():
+    return using_config('enable_backward', False)
