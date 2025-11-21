0f3dbd5032e280a49a59e8782662fc71058f587e
zer0san <1323639983@qq.com>
2025-11-21 11:00:42 +0800

    添加更多运算符

--- src/Operations.py
+++ src/Operations.py
@@ -1,6 +1,7 @@
 from src.core import Function, as_array
 import numpy as np
 
+
 class Square(Function):
     def forward(self, x):
         y = x ** 2
@@ -31,6 +32,7 @@ class Add(Function):
     def backward(self, gy):
         return gy, gy
 
+
 class Mul(Function):
     def forward(self, x0, x1):
         y = x0 * x1
@@ -41,6 +43,49 @@ class Mul(Function):
         x1 = self.inputs[1].data
         return gy * x1, gy * x0
 
+
+# 相反数
+class Neg(Function):
+    def forward(self, x):
+        return -x
+
+    def backward(self, gy):
+        return -gy
+
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        y = x0 - x1
+        return y
+
+    def backward(self, gy):
+        return gy, -gy
+
+
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0 / x1
+        return y
+
+    def backward(self, gy):
+        x0 = self.inputs[0].data
+        x1 = self.inputs[1].data
+        return gy / x1, gy * (-x0 / x1 ** 2)
+
+
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c
+
+    def forward(self, x):
+        y = x ** self.c
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        c = self.c
+        gx = c * x ** (c - 1) * gy
+        return gx
 
 
 # 为了方便使用，定义函数
@@ -56,6 +101,34 @@ def add(x0, x1):
     x1 = as_array(x1)
     return Add()(x0, x1)
 
+
 def mul(x0, x1):
     x1 = as_array(x1)
     return Mul()(x0, x1)
+
+
+def neg(x):
+    return Neg()(x)
+
+
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0, x1)
+
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)
+
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0)
+
+def pow(x,c):
+    return Pow(c)(x)
--- src/ops_patch.py
+++ src/ops_patch.py
@@ -1,7 +1,7 @@
 # 在此文件中进行运算符重载，避免循环引用
 
 from src.core import Variable
-from src.Operations import add, mul
+from src.Operations import add, mul, neg, sub, rsub, div, rdiv, pow
 
 # Variable.__add__ = lambda self, other: add(self, other)
 # Variable.__mul__ = lambda self, other: mul(self, other)
@@ -11,3 +11,9 @@ Variable.__add__ = add
 Variable.__mul__ = mul
 Variable.__radd__ = add
 Variable.__rmul__ = mul
+Variable.__neg__ = neg
+Variable.__sub__ = sub
+Variable.__rsub__ = rsub
+Variable.__truediv__ = div
+Variable.__rtruediv__ = rdiv
+Variable.__pow__ = pow
