c4aabd9738e7b502fe78d22d8d71c0b8e9aadb51
zer0san <1323639983@qq.com>
2025-11-23 12:32:14 +0800

    增强全连接层，使其可以自动推导输入数

--- src/nn/layer.py
+++ src/nn/layer.py
@@ -5,6 +5,7 @@ import numpy as np
 
 __all__ = ['Linear']
 
+
 class Layer:
     def __init__(self):
         self._params = set()  # 保存所有参数
@@ -36,17 +37,27 @@ class Layer:
 
 # 线性层
 class Linear(Layer):
-    def __init__(self, in_size, out_size, no_bias=False, dtype=np.float32):
+    def __init__(self, out_size, no_bias=False, dtype=np.float32, in_size = None):
         super().__init__()
-        W_data = np.random.randn(in_size, out_size).astype(dtype) * np.sqrt(1 / in_size)
-        self.W = Parameter(W_data, name='W')
+        self.in_size = in_size
+        self.out_size = out_size
+        self.dtype = dtype
+        self.W = Parameter(None, name='W')
+        if self.in_size is not None:
+            self._init_W()
         if no_bias:
             self.b = None
         else:
             b_data = np.zeros(out_size, dtype=dtype)
             self.b = Parameter(b_data, name='b')
 
+    def _init_W(self):
+        W_data = np.random.randn(self.in_size, self.out_size).astype(self.dtype) * np.sqrt(1 / self.in_size)
+        self.W.data = W_data
+
     def forward(self, x):
+        if self.W.data is None:
+            self.in_size = x.shape[1]
+            self._init_W()
         y = layers.linear(x, self.W, self.b)
         return y
-
