705dc386a84b211f9b409889b3e827766c2f9847
zer0san <1323639983@qq.com>
2025-11-24 08:54:52 +0800

    增加切片操作

---
+++ src/core/getitem.py
@@ -0,0 +1,31 @@
+from src.core.function import Function
+import numpy as np
+
+class GetItem(Function):
+    def __init__(self, slices):
+        self.slices = slices
+
+    def forward(self, x):
+        y = x[self.slices]
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        f = GetItemGrad(self.slices, x.shape)
+        return f(gy)
+
+def get_item(x, slices):
+    return GetItem(slices)(x)
+
+class GetItemGrad(Function):
+    def __init__(self, slices, in_shape):
+        self.slices = slices
+        self.in_shape = in_shape
+
+    def forward(self, gy):
+        gx = np.zeros(self.in_shape)
+        np.add.at(gx, self.slices, gy)
+        return gx
+
+    def backward(self, gy):
+        return get_item(gy, self.slices)
