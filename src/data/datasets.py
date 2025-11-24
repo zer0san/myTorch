053c03ede45f52ff6d5e20f4c9b864e3093e9472
zer0san <1323639983@qq.com>
2025-11-24 15:23:41 +0800

    增加大数据类BigDatasets，增加Compose类

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -7,8 +7,8 @@ class Datasets:
     def __init__(self, data, label=None, transform=None, target_transform=None):
         self.transform = transform
         self.target_transform = target_transform
-        self.transform = transform or (lambda x:x)
-        self.target_transform = target_transform or (lambda x:x)
+        self.transform = transform or (lambda x: x)
+        self.target_transform = target_transform or (lambda x: x)
         self.data = data
         self.label = label
 
@@ -20,3 +20,21 @@ class Datasets:
             return self.transform(self.data[idx])
         else:
             return self.transform(self.data[idx]), self.target_transform(self.label[idx])
+
+
+# 专门处理大数据
+class BigDatasets(Datasets):
+    # 不加载全部数据，只保存索引
+    def __init__(self, index_list, loader_func, label_loader_func=None, transform=None, target_transform=None):
+        super().__init__(data=index_list, label=None, transform=transform, target_transform=target_transform)
+        self.loader_func = loader_func
+        self.label_loader_func = label_loader_func
+
+    def __getitem__(self, idx):
+        index = self.data[idx]
+        x = self.transform(self.loader_func(index))
+        if self.label_loader_func is None:
+            return x
+
+        y = self.target_transform(self.label_loader_func(index))
+        return x, y
--- src/data/transforms.py
+++ src/data/transforms.py
@@ -1,6 +1,17 @@
 import numpy as np
 
-__all__ = ['Normalize','Flatten','AsType']
+__all__ = ['Normalize','Flatten','AsType','Compose']
+
+class Compose:
+    def __init__(self, transforms = []):
+        self.transforms = transforms
+
+    def __call__(self, x):
+        if not self.transforms:
+            return x
+        for t in self.transforms:
+            x = t(x)
+        return x
 
 class Normalize:
     def __init__(self, mean=0, std=1):
