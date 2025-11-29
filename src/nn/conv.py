f63b20648672de98cb912aba1acbd1d4b2957723
zer0san <1323639983@qq.com>
2025-11-29 16:36:49 +0800

    实现最大池化

--- src/nn/conv.py
+++
@@ -1,3 +0,0 @@
-
-def get_conv_outsize(input_size, kernel_size, stride, pad):
-    return (input_size + pad * 2 - kernel_size ) // stride + 1
--- src/nn/layers.py
+++ src/nn/layers.py
@@ -1,6 +1,7 @@
 from src.core import Function, sum_to, img2col, col2img, Variable
 from src.cuda import cuda
 
+
 # 线性层
 class Linear(Function):
     def forward(self, x, w, b):
@@ -19,6 +20,7 @@ class Linear(Function):
 
 def linear(x, w, b=None):
     return Linear()(x, w, b)
+
 
 class Conv2d(Function):
     def __init__(self, stride, pad):
@@ -64,6 +66,61 @@ class Conv2d(Function):
 
         return dx, dW, db
 
+
 def conv2d(x, W, b=None, stride=1, pad=0):
     return Conv2d(stride, pad)(x, W, b)
 
+
+class MaxPool2d(Function):
+    def __init__(self, kernel_size, stride=1, pad=0):
+        super().__init__()
+        if isinstance(kernel_size, int):
+            kernel_size = (kernel_size, kernel_size)
+        else:
+            assert len(kernel_size) == 2
+        self.kh, self.kw = kernel_size
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        xp = cuda.get_array_module(x)
+        N, C, H, W = x.shape
+        kh, kw = self.kh, self.kw
+        sy, sx = self.stride, self.stride
+        ph, pw = self.pad, self.pad
+
+        x_pad = xp.pad(x, ((0, 0), (0, 0), (ph, ph), (pw, pw))) if ph or pw else x
+        Hp, Wp = x_pad.shape[2:]
+
+        out_h = (Hp - kh) // sy + 1
+        out_w = (Wp - kw) // sx + 1
+
+        col = img2col(x_pad, (kh, kw), sy, 0)[0]  # 只取 cols
+        col = col.reshape(N, C, out_h, out_w, kh * kw)
+
+        # 按通道独立找最大值
+        self.max_idx = xp.argmax(col, axis=-1)  # (N,C,OH,OW)
+        out = xp.max(col, axis=-1)
+
+        self.cache = (x.shape, (N, C, Hp, Wp), out_h, out_w, col.shape)
+        return out
+
+    def backward(self, gy):
+        xp = cuda.get_array_module(gy)
+        x_shape, pad_shape, out_h, out_w, col_shape = self.cache
+        N, C, Hp, Wp = pad_shape
+
+        dcol = xp.zeros(col_shape, dtype=gy.data.dtype)
+        dcol[xp.arange(N)[:, None, None, None],
+        xp.arange(C)[None, :, None, None],
+        xp.arange(out_h)[None, None, :, None],
+        xp.arange(out_w)[None, None, None, :],
+        self.max_idx] = gy.data
+
+        dx = col2img(dcol.reshape(-1, self.kh * self.kw), pad_shape, (self.kh, self.kw), self.stride, 0)
+        if self.pad > 0:
+            dx = dx[:, :, self.pad:-self.pad, self.pad:-self.pad]
+        return dx
+
+def max_pool2d(x,ksize,stride=1,pad=0):
+    return MaxPool2d(ksize,stride,pad)(x)
